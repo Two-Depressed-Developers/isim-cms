8ffebeec47f6190dfe63c29fc2c8f2cd8a690134
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2025-12-21 15:30:02 +0100

    Classrooms

---
+++ strapi/src/api/classroom-resources/services/classroom-resources.ts
@@ -0,0 +1,7 @@
+/**
+ * classroom-resources service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::classroom-resources.classroom-resources');
