7a22748f59e08d3ce980096d0f605e53d7f2142b
Michał Ślęzak <michaldakol@gmail.com>
2025-12-26 23:42:07 +0100

    Feat: Multi role for end user

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,20 +1,51 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+    /**
+     * An asynchronous register function that runs before
+     * your application is initialized.
+     *
+     * This gives you an opportunity to extend code.
+     */
+    register(/* { strapi }: { strapi: Core.Strapi } */) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+    /**
+     * An asynchronous bootstrap function that runs before
+     * your application gets started.
+     *
+     * This gives you an opportunity to set up your data model,
+     * run jobs, or perform some special logic.
+     */
+    async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+        const rolesToCreate = [
+            {
+                name: "StaffMember",
+                description:
+                    "Role for members of the department. Allows you to edit your profile",
+            },
+            {
+                name: "PanelAdmin",
+                description: "Panel Admin",
+            },
+            {
+                name: "Helpdesk",
+                description: "Helpdesk role",
+            },
+        ];
+
+        const existingRoles = await strapi
+            .query("plugin::users-permissions.role")
+            .findMany();
+
+        for (const roleData of rolesToCreate) {
+            const exists = existingRoles.find((r) => r.name === roleData.name);
+            if (!exists) {
+                const role = await strapi
+                    .query("plugin::users-permissions.role")
+                    .create({ data: roleData });
+
+                console.log(`Created role: ${role.name}`);
+            }
+        }
+    },
 };
