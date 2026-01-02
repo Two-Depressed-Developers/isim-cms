dba28730c0c9d32800122923fba68828eeb2557a
Dominik Sieroń <71605737+CALLmeDOMIN@users.noreply.github.com>
2026-01-02 22:32:58 +0100

    Consultation bookings improved

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -63,6 +63,7 @@ export interface MembersCompConsultationAvailability
     durationMinutes: Schema.Attribute.Integer & Schema.Attribute.DefaultTo<10>;
     endTime: Schema.Attribute.Time;
     isActive: Schema.Attribute.Boolean;
+    maxAttendees: Schema.Attribute.Integer;
     startTime: Schema.Attribute.Time;
   };
 }
