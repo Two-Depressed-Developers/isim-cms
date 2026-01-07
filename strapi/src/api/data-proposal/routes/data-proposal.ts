a4f5506bf2defc2098d6d44a61f2d1f1770abaa3
Michał Ślęzak <michaldakol@gmail.com>
2026-01-07 17:50:06 +0100

    fix: secure data-proposal with api-secret-key

--- strapi/src/api/data-proposal/routes/data-proposal.ts
+++ strapi/src/api/data-proposal/routes/data-proposal.ts
@@ -2,6 +2,12 @@
  * data-proposal router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::data-proposal.data-proposal');
+export default factories.createCoreRouter("api::data-proposal.data-proposal", {
+    config: {
+        create: {
+            policies: ["global::has-api-key"],
+        },
+    },
+});
